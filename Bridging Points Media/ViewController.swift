6441bce941984d96c0f8d2fae95a6da36a52c684
Likhit Garimella <likhitgmella@gmail.com>
2021-04-18 02:33:11 +0530

    home screen webview done; sb tab bar icons done

--- Bridging Points Media/ViewController.swift
+++ Bridging Points Media/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webview = WKWebView(frame: view.frame)
+        view.addSubview(webview)
         
+        let url = URL(string: "https://www.bridgingpointsmedia.com")!
+        let request = URLRequest(url: url)
+        webview.load(request)
         
     }
     
-}   // #20
+}   // #26

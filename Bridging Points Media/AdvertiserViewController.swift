2837021412c3eaedd06e088c1ae45e8ee8e81f08
Likhit Garimella <likhitgmella@gmail.com>
2021-04-18 16:11:13 +0530

    advertiser & publisher screens web view done

--- Bridging Points Media/AdvertiserViewController.swift
+++ Bridging Points Media/AdvertiserViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class AdvertiserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webview = WKWebView(frame: view.frame)
+        view.addSubview(webview)
         
+        let url = URL(string: "https://www.bridgingpointsmedia.com/advertiser-signup/")!
+        let request = URLRequest(url: url)
+        webview.load(request)
         
     }
     
-}   // #20
+}   // #26

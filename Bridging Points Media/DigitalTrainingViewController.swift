c039f4b60ec6e5543b412a3c73507d258b5b2816
Likhit Garimella <likhitgmella@gmail.com>
2021-04-18 14:43:45 +0530

    digital training screen web view done

--- Bridging Points Media/DigitalTrainingViewController.swift
+++ Bridging Points Media/DigitalTrainingViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class DigitalTrainingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webview = WKWebView(frame: view.frame)
+        view.addSubview(webview)
         
+        let url = URL(string: "https://trainings.bpmpl.com")!
+        let request = URLRequest(url: url)
+        webview.load(request)
         
     }
     
-}   // #20
+}   // #26

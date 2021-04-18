ceb7df2655fddb22acabaedc0737ff5b04249648
Likhit Garimella <likhitgmella@gmail.com>
2021-04-18 16:04:55 +0530

    ads sub vc files added; classes in sb

---
+++ Bridging Points Media/AdvertiserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AdvertiserViewController.swift
+//  Bridging Points Media
+//
+//  Created by Likhit Garimella on 18/04/21.
+//
+
+import UIKit
+
+class AdvertiserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
---
+++ Bridging Points Media/PublisherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PublisherViewController.swift
+//  Bridging Points Media
+//
+//  Created by Likhit Garimella on 18/04/21.
+//
+
+import UIKit
+
+class PublisherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20

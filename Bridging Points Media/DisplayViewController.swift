85cc9c6348d5b110f1b59f65aae36f4ee78d2a40
Likhit Garimella <likhitgmella@gmail.com>
2021-04-20 00:09:59 +0530

    2 new sbs & new vc files under advertising

---
+++ Bridging Points Media/DisplayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DisplayViewController.swift
+//  Bridging Points Media
+//
+//  Created by Likhit Garimella on 20/04/21.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
---
+++ Bridging Points Media/PrintViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PrintViewController.swift
+//  Bridging Points Media
+//
+//  Created by Likhit Garimella on 20/04/21.
+//
+
+import UIKit
+
+class PrintViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20

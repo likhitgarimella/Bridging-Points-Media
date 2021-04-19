7dfc33728f99be4c28932274811d0219c43a9f3a
Likhit Garimella <likhitgmella@gmail.com>
2021-04-19 23:23:45 +0530

    marketing all 10 sub sec screens web view done

--- Bridging Points Media/WhatsappViewController.swift
+++ Bridging Points Media/WhatsappViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class WhatsappViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webview = WKWebView(frame: view.frame)
+        webview.translatesAutoresizingMaskIntoConstraints = false
+        webview.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view.addSubview(webview)
         
+        let url = URL(string: "https://www.bridgingpointsmedia.com/services/whatsapp-marketing/")!
+        let request = URLRequest(url: url)
+        webview.load(request)
         
     }
     
-}   // #20
+}   // #28
